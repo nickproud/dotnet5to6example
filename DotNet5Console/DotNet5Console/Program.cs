8b70a607293fe54a7653aa5542243453472a7783
Nick Proud <nickpproud@gmail.com>
2022-04-03 14:59:08 +0100

    Created .net 5 console
    Outputs the current .net version

---
+++ DotNet5Console/DotNet5Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet5Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"This app is running: {Environment.Version}");
+        }
+    }
+}
